7cdf23a53abdbd0e8c2eea3298994c35411f0e29
kkomin <100822055+kkomin@users.noreply.github.com>
2024-06-03 19:41:30 +0900

    [FEAT] : ⚙️발사체 & 발사 구현
    - 발사체 prefab 구현
    - 발사 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,41 +7,49 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     private bool isShooting = false;
-    private bool wasWalkingBeforeShooting = false;
+    public Transform firePoint;
+    public GameObject bulletPrefab;
 
     void Update()
     {
         bool isWalking = Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0;
         animator.SetBool("isWalk", isWalking);
 
-        if (!isShooting)
-        {
-            animator.SetBool("isWalk", isWalking);
-        }
-
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             StartCoroutine(Shoot());
         }
 
-        if(!isWalking)
+        if (!isWalking && !isShooting)
         {
             animator.SetTrigger("isIdle");
         }
-
     }
+
     IEnumerator Shoot()
     {
         isShooting = true;
+
         while (Input.GetMouseButton(0))
         {
             animator.SetTrigger("isShoot");
             Debug.Log("Shooting");
 
-            yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
-        }
-        isShooting = false;
+            // 총알 생성 및 발사
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            rb.AddForce(firePoint.forward * 1000f);
 
+            // 발사 애니메이션이 끝날 때까지 대기
+            yield return new WaitForSeconds(0.5f);
+
+            if (!Input.GetMouseButton(0))
+            {
+                break;
+            }
+        }
+
+        isShooting = false;
         animator.SetTrigger("isIdle");
         Debug.Log("Shoot finished");
     }
